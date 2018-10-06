f092020059f8fae25052552ca17b2bb4728afdae
Selvin Norman <selvin715@gmail.com>
2018-10-05 17:09:23 -0700

    got Check for palindromes done

---
+++ queue/buildQueue.js
@@ -0,0 +1,28 @@
+'use strict';
+const _Node = require('./buildNode')
+
+class Queue {
+  constructor() {
+      this.first = null;
+      this.last = null;
+  }
+  enqueue(data) {
+  //create a node with the data that you want to add to the queue
+  const node = new _Node(data);
+
+  //if the queue is empty, 
+  //make the node the first node on the queue
+  if (this.first === null) {
+      this.first = node;
+  }
+  //if there is something on the queue already
+  //then take the node that is currently at the end of the queue
+  //and link it to the new node
+  if (this.last) {
+      node.next = this.last;
+      this.last.prev = node;
+  }
+  //make the new node the last item on the queue
+  this.last = node;
+  }
+}
---
+++ stack/drills.js
@@ -0,0 +1,91 @@
+'use strict';
+const stackClass = require('./buildStack');
+
+
+function main() {
+  let starTrek = new stackClass();
+
+  starTrek.push ('Kirk');
+  starTrek.push ('Spock');
+  starTrek.push ('McCoy');
+  starTrek.push ('Scotty');
+
+  //console.log(JSON.stringify(starTrek)); 
+
+  function peek(stk) {
+    console.log(stk.top.data)
+  }
+  
+  function display(stk) {
+    //base case
+    let tempNode = stk.top;
+    while (tempNode !== null) {
+      console.log(tempNode.data);
+      tempNode = tempNode.next;
+    }  
+  }  
+  
+  function remove(stk, item) {
+    //base case
+    let tempNode = stk.top;
+    let prevNode;
+    if (stk.top === null) {
+      return null;
+    }
+    while (tempNode.data !== item && tempNode.next !== null) {
+      prevNode = tempNode;
+      tempNode = tempNode.next;
+    }
+    if (tempNode.next === null) {
+      return new Error('Item not found!');
+    }
+    prevNode.next = prevNode.next.next;
+    console.log(JSON.stringify(starTrek));
+  }  
+
+  //peek(starTrek);
+  //display(starTrek);
+  console.log(remove(starTrek, 'McCoy'));
+}
+//main();
+
+function is_palindrome(s) {
+  let forwardStack = new stackClass;
+  let reverseStack = new stackClass;
+  s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  twoStack(s);
+  // if (forwardStack ===  reverseStack) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+
+  function twoStack(str) {
+   
+    // base case 
+    if (str.length < 1) {
+
+      return;
+    }
+    //recursive case
+    forwardStack.push(str[0]);
+
+    //console.log('forwardStack: ', JSON.stringify(forwardStack));
+    reverseStack.push(str[str.length-1])
+
+
+    twoStack(str.slice(1, str.length-1));
+
+  }
+  if (forwardStack.data == reverseStack.data) {
+    return true;
+  } 
+}
+
+
+
+// true, true, true'
+console.log(is_palindrome("dad"));
+console.log(is_palindrome("A man, a plan, a canal: Panama"));
+console.log(is_palindrome("1001"));
+console.log(is_palindrome("Tauhida"));
